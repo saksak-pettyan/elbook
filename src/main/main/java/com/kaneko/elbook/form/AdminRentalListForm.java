867fac51569822332106baaaa6bf28e216819870
saksak-pettyan <pettyan0118@gmail.com>
2025-12-23 11:17:46 +0900

    Delete src directory

--- src/main/main/java/com/kaneko/elbook/form/AdminRentalListForm.java
+++
@@ -1,9 +0,0 @@
-package com.kaneko.elbook.form;
-
-import lombok.Data;
-
-@Data
-public class AdminRentalListForm {
-	private String searchUserName; // 部分一致
-	private String searchBookName; // 部分一致
-}
